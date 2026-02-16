69ed842ed98afc8502e46b9774136171408c2123
Aaron <33184112+astark929@users.noreply.github.com>
2026-02-16 14:20:40 -0800

    Update body.ino

--- body/body.ino
+++ body/body.ino
@@ -11,18 +11,17 @@ the point of the body should be:
 */
 
 #include "variable.h"
+#include "sensors.h"
 #include "movement.h"
 #include <SoftwareSerial.h>
 //check the above library, it may be only for arduino uno
 
 using namespace std;
 
-SoftwareSerial espSerial(10, 11); //initalizing ports to communicate with esp
 movement m;
+sensors s;
 
 void setup(){
-  //motor_start();
-  sensor_start();
   /*
   esp ports 10 and 11 
   10 -> esp TX    recieves info
@@ -35,7 +34,6 @@ void setup(){
   
   */
   Serial.begin(9600); //communicating with USB for debugging
-  espSerial.begin(115200); //esp communication
 
 }
 
@@ -48,7 +46,7 @@ void loop(){
   */
 
 
-  while(!espSerial.available()){
+  while(!s.espAvailable()){
     //waits for esp response
     //the while statement will skip once there is data
     //  for the esp
@@ -68,77 +66,7 @@ void loop(){
   */
 
 }
-/*
-void motor_start(){
-  pinMode(RightMotorDirPin1, output); 
-  pinMode(RightMotorDirPin2, output); 
-  pinMode(speedPinL, output);  
- 
-  pinMode(LeftMotorDirPin1, output);
-  pinMode(LeftMotorDirPin2, output); 
-  pinMode(speedPinR, output);
-  pinMode(RightMotorDirPin1B, output); 
-  pinMode(RightMotorDirPin2B, output); 
-  pinMode(speedPinLB, output);  
- 
-  pinMode(LeftMotorDirPin1B, output);
-  pinMode(LeftMotorDirPin2B, output); 
-  pinMode(speedPinRB, output);
-}
-*/
-void sensor_start(){
-  pinMode(Trig_PIN, output); 
-  pinMode(Echo_PIN,input);
-  //ultra sonic
 
-  pinMode(sensor1, INPUT);
-  pinMode(sensor2, INPUT);
-  pinMode(sensor3, INPUT);
-  pinMode(sensor4, INPUT);
-  pinMode(sensor5, INPUT);
-  //line detector
-
-}
-
-void line(){
-  String senstr="";
-  int linecount = 0;
-  int s0 = !digitalRead(sensor1);
-  int s1 = !digitalRead(sensor2);
-  int s2 = !digitalRead(sensor3);
-  int s3 = !digitalRead(sensor4);
-  int s4 = !digitalRead(sensor5);
-  int sensorvalue=32;
-  sensorvalue +=s0*16+s1*8+s2*4+s3*2+s4;
-  senstr = String(sensorvalue,BIN);
-  senstr = senstr.substring(1,6);
-  
-  /*
-  Serial.print(senstr);
-  Serial.print("\t");
-  these are debugger statements right now
-  */
-
-  for(int count: senstr){
-    if(count == '1'){
-      linecount++;
-    }
-  }
-  if(linecount == 1){
-    espSerial.print("1 line");
-  }
-  else if(linecount == 2){
-    espSerial.print("2 line");
-  }
-  else if(linecount == 3){
-    espSerial.print("3 line");
-  }
-  else{
-    espSerial.print("0 line");
-  }
-
-}
-//for the line sensing
 
 
 
