9a4175273018931bce2f5b28e242e3a7ec02114b
Jose C <50343694+Jose-A-C@users.noreply.github.com>
2026-03-23 19:21:05 -0700

    Add sensors.h header file for sensor class

---
+++ final/sensors.h
@@ -0,0 +1,26 @@
+
+#ifndef SENSORS_H
+#define SENSORS_H
+#pragma once
+
+#include "variable.h"
+
+extern SoftwareSerial espSerial;
+
+class sensors {
+  public:
+    sensors::sensors();
+    float getdistance();
+    void line();
+    bool espAvailable();
+    int espRead();
+    void Sangle(int a);
+    bool lidar_left();
+    void lidar_right();
+    String lidar_detect();
+    
+  private:
+    //SoftwareSerial espSerial(10, 11); //initalizing ports to communicate with esp
+};
+
+#endif
