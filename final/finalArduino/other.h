54a5f3235a6ce296805ec23d4ee90393ce291b2a
Aaron <33184112+astark929@users.noreply.github.com>
2026-03-25 09:46:18 -0700

    Add files via upload

---
+++ final/finalArduino/other.h
@@ -0,0 +1,16 @@
+#ifndef OTHER_H
+#define OTHER_H
+#pragma once
+
+#include "variable.h"
+
+class other {
+  public:
+    other::other();
+    void lightup(char s, int v);
+
+  private: 
+};
+
+
+#endif
